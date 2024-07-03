f39ede90cbb7c12a9c41bf02b714b57dbae7aab6
Fernando Vega <normacocsub@gmail.com>
2024-07-02 23:27:45 -0500

    first commit

---
+++ src/test/java/com/challenge_literalura/challenge_literalura/ChallengeLiteraluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge_literalura.challenge_literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeLiteraluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
